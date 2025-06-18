8fccf83088a8740eb2fca56e1fdfa778c6191de5
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-06-18 16:49:32 +0530

    Patterns Learmings

--- 1_Basics of Programming/2_PatternsinCPP.cpp
+++ 1_Basics of Programming/2_PatternsinCPP.cpp
@@ -11,37 +11,98 @@
 using namespace std;
 int main()
 {
-    // Nested Loops -
+    // Nested Loops  & Patterns -
 
     /*
-    Print the Pattern -
+    1) Print the Nested Loop Pattern -
     1 1 1 1
     2 2 2 2
     3 3 3 3
     4 4 4 4
      */
 
+    // int n;
+    // cout << "n's value " << endl;
+    // cin >> n;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /* O/p -
+
+    n's value
+    6
+    1 1 1 1 1 1
+    2 2 2 2 2 2
+    3 3 3 3 3 3
+    4 4 4 4 4 4
+    5 5 5 5 5 5
+    6 6 6 6 6 6
+     */
+    // ____________ ____________
+
+    // /*  2) Print the Star Pattern
+    // *
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+    //  */
+    //     int n;
+    //     cout << "vakue of n - " << endl;
+    //     cin >> n;
+
+    //     for (int i = 1; i <= n; i++)
+    //     {
+    //         for (int j = 1; j <= i; j++)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         cout << endl;
+    //     }
+    // /*
+    // 5
+    // *
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+    //  */
+
+    // ____________ ____________
+
+    /*  3) Print the Inverted Star Pattern
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+     */
     int n;
-    cout << "n's value " << endl;
+    cout << "vakue of n - " << endl;
     cin >> n;
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= n-i+1; j++)
         {
-            cout << i << " ";
+            cout << "* ";
         }
         cout << endl;
     }
-/* O/p - 
-
-n's value
-6
-1 1 1 1 1 1 
-2 2 2 2 2 2 
-3 3 3 3 3 3 
-4 4 4 4 4 4 
-5 5 5 5 5 5 
-6 6 6 6 6 6 
+/* 
+vakue of n - 
+5
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
  */    
 }
