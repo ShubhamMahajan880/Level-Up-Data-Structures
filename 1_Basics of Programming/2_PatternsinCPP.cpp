9e11293de5a0d521ab61eee50104d910a0037170
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-06-18 13:07:26 +0530

    Starting

---
+++ 1_Basics of Programming/2_PatternsinCPP.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include<algorithm>
+// #include<climits>
+// #include<string>
+// #include<cctype>
+// #include<vector>
+// #include<set>
+// #include<iomanip>
+// #include<cmath>
+using namespace std;
+int main()
+{
+    // Nested Loops -
+
+    /*
+    Print the Pattern -
+    1 1 1 1
+    2 2 2 2
+    3 3 3 3
+    4 4 4 4
+     */
+
+    int n;
+    cout << "n's value " << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
