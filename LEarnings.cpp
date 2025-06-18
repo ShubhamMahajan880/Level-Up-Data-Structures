13a4df357a17d96fc986e522caae73ca7ebad414
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-06-18 21:17:48 +0530

    Completed Patterns

---
+++ LEarnings.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include<algorithm>
+// #include<climits>
+// #include<string>
+// #include<cctype>
+// #include<vector>
+// #include<set>
+// #include<iomanip>
+// #include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "vakue of n - " << endl;
+    cin >> n;
+
+    
+    // 1st Butterfly
+    // Printing Star
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+
+        // Printing SPace
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            cout << "  ";
+        }
+
+        // Prinmting Start for last
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    // 2nd Half Butterfly
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+
+        // Printing SPace
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            cout << "  ";
+        }
+
+        // Prinmting Start for last
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
