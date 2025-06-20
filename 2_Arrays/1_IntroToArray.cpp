48c58b27362adb9417ff815d3be43385194bc92a
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-06-20 13:45:26 +0530

    Arrau Continue

--- 2_Arrays/1_IntroToArray.cpp
+++ 2_Arrays/1_IntroToArray.cpp
@@ -11,5 +11,35 @@
 using namespace std;
 int main()
 {
-    
+    /*
+    Arrays in cpp -  Linear Collection of Same Type of ELements that are stored together in
+    Contiguos Memory Spaces.
+     */
+    // Initliazearray without any value
+    int marks[50];             // 0 to 49
+    cout << marks[0] << endl;  // 4199136 - whn we don;t innitialize the array with any value it takes any garbage value in the memory
+    cout << marks[1] << endl;  // 17566816
+    cout << marks[2] << endl;  // 17566816
+    cout << marks[50] << endl; // 4201056
+
+    int percentage[5] = {0, 1, 2, 3, 4};
+    cout << percentage << endl;
+    cout << sizeof(percentage) << endl;                                          // 20 - int size = 4 bytes , so 5 int = 4*5 = 20 bytes
+    cout << "lenght of array is - " << sizeof(percentage) / sizeof(int) << endl; // lenght of array is - 5
+
+    // Ouput & input of an array -
+    int arr[5] = {0, 1, 2, 3, 4};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << " "; // 0 1 2 3 4
+    }
+    cout << endl;
+
+    int len = sizeof(arr) / sizeof(int);
+
+    for (int idx = 0; idx <= len - 1; idx++)
+    {
+        cout << arr[idx] << " ";//0 1 2 3 
+    }
+    cout << endl;
 }
--- LEarnings.cpp
+++ LEarnings.cpp
@@ -9,47 +9,7 @@
 // #include<iomanip>
 // #include<cmath>
 using namespace std;
-
-int Prod(int a, int b = 2)
-{
-    int theMultiplicaiton = a * b;
-    return theMultiplicaiton;
-}
-
-bool isprime(int n)
-{
-
-    for (int i = 2; i < sqrt(n); i++)
-    {
-        if (n % 2 == 0)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-    return isprime;
-}
-
-int factorial(int c)
-{
-    int fact = 1;
-    for (int i = 1; i <= c; i++)
-    {
-        fact *= i;
-    }
-    cout<<"Factorial of no. - "<<c<<" is - "<<fact<<endl;
-    return fact;
-}
 int main()
 {
-    cout << Prod(5) << endl;     // 10 - by using default defined parametr 2 with 1 argumenet
-    cout << Prod(5, 10) << endl; // 50 - by using both  parameters with both arguements
-    cout << isprime(23) << endl; // 1
-    cout << isprime(2) << endl;  // 1 - Prime Numbe
-    cout << isprime(8) << endl;  // 0 - Not a prime number
-    cout << factorial(5) << endl;//120
-    factorial(10);//Factorial of no. - 10 is - 3628800
+    
 }
